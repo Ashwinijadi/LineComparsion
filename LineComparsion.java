c89294879fb6e892ca840a55fe222fc3c6eef09a
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-09-27 22:39:22 +0530

    added: Comparsion of lines is checked

--- LineComparsion.java
+++ LineComparsion.java
@@ -22,11 +22,12 @@ public class LineComparsion {
     //Comparsion
     Double obj1=new Double(Line1);
     Double obj2=new Double(Line2);
-    boolean compareValue =obj1.equals(obj2);
-    if(compareValue) {
-	System.out.println("Both lines are equal");
-      }
-    else {
-    System.out.println("Both lines are equal");}
+    int compareValue =obj1.compareTo(obj2);
+    if(compareValue==1)
+    { System.out.println("Line1 is greater than line2");}
+    else if(compareValue==-1)
+    { System.out.println("Line1 is less than line2");}
+    else
+    { System.out.println("Line1 is equal to line2");}
     }
     }
